85b96384efc8d139f47e1719726def88d58b5a48
Shivam Kumar <114799228+kumarshivam27december@users.noreply.github.com>
2023-10-12 16:47:05 +0530

    Add files via upload

--- 00_intro.cpp
+++ 00_intro.cpp
@@ -1,27 +1,6 @@
-#include <iostream>
-#include <cstring>
+#include <iostream> 
 using namespace std;
-class Hero{
-    //attributes
-    public:
-    int health;
-    char name[10];
-    char level;
-    void print(){
-        cout<<"level is :"<<level<<endl;
-    }
-};
 int main()
 {
-    Hero shivam;
-
-    //setting the values 
-    shivam.health=100;
-    ////shivam.name="shiv";
-    strcpy(shivam.name, "shivam");
-    shivam.level='A';
-    cout<<"Health of shivam is :"<<shivam.health<<endl;
-    cout<<"Name of shivam is : "<<shivam.name<<endl;
-    cout<<"level of shivam is : "<<shivam.level<<endl;
     return 0;
 }
